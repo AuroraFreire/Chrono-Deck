46772108aa2477c4e89080b2c60336e4735516b7
LourencoFreire <lourencofreire3@gmail.com>
2026-01-25 23:28:07 +0000

    so... egypt sound, pause menu and skipping cutscene system all added

---
+++ scenes/main.gd
@@ -0,0 +1,24 @@
+extends Control
+
+@onready var options: Panel = $Options
+
+func _ready() -> void:
+	options.visible = false
+	options.process_mode = Node.PROCESS_MODE_ALWAYS
+
+func _process(delta: float) -> void:
+	pass
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("PauseMenu"):
+		options.visible = true
+		get_tree().paused = true
+
+func _on_back_pressed() -> void:
+	options.visible = false
+	get_tree().paused = false
+
+
+func _on_main_menu_pressed() -> void:
+	get_tree().paused = false
+	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
--- scripts/first_scene.gd
+++ scripts/first_scene.gd
@@ -74,3 +74,7 @@ func _on_pause_finished():
 
 func _process(delta: float) -> void:
 	pass
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("SkipScene"):
+		get_tree().change_scene_to_file("res://scenes/main.tscn")
